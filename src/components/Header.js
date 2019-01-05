7d4cd8d11e4de4dac0f7021957a25578c02b8704
Ayana Powell <ayanapowell1@gmail.com>
2019-01-05 14:36:42 -0800

    Animate navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,19 +9,7 @@ const HeaderEl = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 60px 0;
-  nav {
-    display: flex;
-  }
-  .nav__item {
-    padding: 0 35px;
-    &:first-of-type {
-      padding-left: 0;
-    }
-  }
-  .nav__list-main {
-    display: flex;
-    padding: 0 180px;
-  }
+  position: relative;
 
   .contact-info {
     font-family: ${FF.secondary};
@@ -29,6 +17,40 @@ const HeaderEl = styled.div`
     font-weight: 100;
     font-size: 12px;
     letter-spacing: 1px;
+  }
+`
+
+const NavEl = styled.nav`
+  display: flex;
+  .nav-list__menu-icon {
+    position: fixed;
+    left: 45px;
+    width: 50px;
+    height: 50px;
+    background-color: white;
+    border-radius: 50%;
+    transform: scale(0, 0);
+    opacity: 0;
+  }
+  &.nav-list--default {
+    .nav-list__item {
+      padding: 0 35px;
+      &:first-of-type {
+        padding-left: 0;
+      }
+    }
+    .nav-list__list {
+      display: flex;
+      padding: 0 180px;
+    }
+  }
+
+  &.nav-list--alt {
+    .nav-list__menu-icon {
+    }
+    .nav-list__inner {
+      display: none;
+    }
   }
 `
 
@@ -49,11 +71,17 @@ class Header extends Component {
           navIsHidden: true,
         })
         TweenMax.to('.site-name', 0.5, { top: -150 })
+        TweenMax.to('.nav-list__menu-icon', 0.4, {
+          css: { opacity: 1, scaleX: 1, scaleY: 1 },
+        })
       } else if (topPos === 0) {
         this.setState({
           navIsHidden: false,
         })
-        TweenMax.to('.site-name', 0.3, { top: 60 })
+        TweenMax.to('.site-name', 0.3, { top: 60, delay: 0.2 })
+        TweenMax.to('.nav-list__menu-icon', 0.2, {
+          css: { scaleX: 0, scaleY: 0 },
+        })
       }
     }
   }
@@ -65,22 +93,29 @@ class Header extends Component {
           id="header"
         >
           <SiteName />
-          <nav>
-            <div className="nav__list-main">
-              <div className="nav__item">
-                <p>Work</p>
-              </div>
-              <div className="nav__item">
-                <p>About</p>
-              </div>
-              <div className="nav__item">
-                <p>Contact</p>
-              </div>
-              <div className="nav__item">
-                <p>Photography</p>
+          <NavEl
+            className={
+              this.state.navIsHidden ? 'nav-list--alt' : 'nav-list--default'
+            }
+          >
+            <div className="nav-list__menu-icon" />
+            <div className="nav-list__inner">
+              <div className="nav-list__list">
+                <div className="nav-list__item">
+                  <p>Work</p>
+                </div>
+                <div className="nav-list__item">
+                  <p>About</p>
+                </div>
+                <div className="nav-list__item">
+                  <p>Contact</p>
+                </div>
+                <div className="nav-list__item">
+                  <p>Photography</p>
+                </div>
               </div>
             </div>
-          </nav>
+          </NavEl>
           <div className="contact-info">
             <a href="mailto:ayanapowell1@gmail.com?Subject=Hey%20Ayana">
               E: ayanapowell1@gmail.com
