23d06e59609e54c385a05a7cc5d3237f959891db
Ayana Powell <ayanapowell1@gmail.com>
2019-08-08 12:05:49 -0400

    Build and style Intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
 import vars from '../utils/emotionVars';
-import '../styles/variables.scss';
 import Container from './Container';
 
+const IntroEl = styled.div`
+  font-family: ${vars.ff_primary};
+  margin-top: 100px;
+  .intro__title,
+  .intro__subtitle {
+    font-weight: 300;
+    position: relative;
+  }
+  .intro__title {
+    font-size: 70px;
+    line-height: 126px;
+    .overlay {
+      margin-top: 13px;
+    }
+  }
+
+  .intro__subtitle {
+    font-size: 60px;
+    margin-top: -20px;
+    .overlay {
+      margin-top: 22px;
+    }
+  }
+  .intro__description {
+    font-size: 30px;
+    line-height: 1.4;
+    font-weight: 300;
+    margin-top: 15px;
+    button {
+      font-size: 25px;
+      font-weight: 300;
+      padding: 0;
+      margin-top: 50px;
+      display: block;
+    }
+  }
+`;
 const Intro = () => {
   return (
     <div>
       <Container>
-        <div className="col-sm-9 px-0">
+        <IntroEl className="col-sm-9 px-0">
           <h1 className="intro__title">
             <span>Ayana Powell</span>
           </h1>
@@ -21,7 +56,7 @@ const Intro = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
             <button>View my work</button>
           </p>
-        </div>
+        </IntroEl>
       </Container>
     </div>
   );
