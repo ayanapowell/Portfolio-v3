fe8502c4c2e7a47c242ddfb6049c78b4af0c1b7e
Ayana Powell <ayanapowell1@gmail.com>
2020-03-18 11:41:37 -0400

    WIP - circle animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { TweenMax } from 'gsap';
 import { Link } from 'gatsby';
 import vars from '../utils/emotionVars';
 import '../styles/variables.scss';
 import Container from './Container';
 import ProjectNav from './ProjectNav';
-import SiteName from './SiteName';
 
 const HeaderEl = styled.div`
   position: relative;
@@ -14,20 +14,21 @@ const HeaderEl = styled.div`
   align-items: center;
   .circle {
     display: flex;
+    justify-content: center;
+    align-items: center;
     position: absolute;
     left: 50px;
     width: 50px;
     height: 50px;
-    padding: 12px 0;
+    padding: 15px 0;
     border-radius: 50%;
     background: #f0f0f0;
 
-    &::after {
-      content: '';
+    &__inner {
       height: 100%;
       width: 1px;
-      margin: 0 auto;
       background: black;
+      margin: 0 4px;
     }
   }
   .container--header {
@@ -58,16 +59,35 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isHidden: true,
+      navIsOpen: false,
+      isHovered: false,
     };
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+  }
+  componentDidUpdate() {
+    if (this.state.navIsOpen) {
+      TweenMax.to('.circle__inner', 0.4, {
+        margin: 0,
+      });
+
+      TweenMax.to('.circle__inner--left', 0.4, {
+        color: 'red',
+      });
+      TweenMax.to('.circle__inner--right', 0.4, {
+        color: 'blue',
+      });
+    }
   }
   toggleMenu() {
-    this.setState({ isHidden: !this.state.isHidden });
+    this.setState({ navIsOpen: !this.state.navIsOpen });
+  }
+  handleHover() {
+    this.setState({ isHovered: !this.state.isHovered });
   }
   render() {
     // Disable scrolling when navigation is open
-    if (!this.state.isHidden) {
+    if (this.state.navIsOpen) {
       document.body.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'auto';
@@ -76,7 +96,15 @@ class Header extends React.Component {
     return (
       <>
         <HeaderEl className="header">
-          <Link to="/" className="circle"></Link>
+          <Link
+            to="/"
+            className="circle"
+            onMouseEnter={this.handleHover}
+            onMouseLeave={this.handleHover}
+          >
+            <span className="circle__inner cicle__inner--left"></span>
+            <span className="circle__inner cicle__inner--right"></span>
+          </Link>
           <Container modifier={this.props.modifier}>
             <ul className="header__nav-list">
               <li>
@@ -84,7 +112,7 @@ class Header extends React.Component {
                   Work
                 </span>
 
-                {!this.state.isHidden && (
+                {this.state.navIsOpen && (
                   <ProjectNav handleNavToggle={this.toggleMenu} />
                 )}
               </li>
