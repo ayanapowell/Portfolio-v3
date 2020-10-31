5d8a14795862cb82a34d71d3fd51675309fe8c1f
Ayana Powell <apowell@phase2technology.com>
2020-10-31 18:23:54 -0400

    Style about and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import vars from '../utils/emotionVars';
 import Container from '../components/Container';
 import Hero from '../components/project/Hero';
 
-const AboutPageEl = styled.div`
-height: 80vh;
-display: flex;
-flex-direction: column;
-justify-content: center;
-width: 50%;
-margin-left: 25%;
-p {
-  font-size: 20px;
-}
+const ContactPageEl = styled.div`
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  margin-left: 25%;
+
 .hero {
   height: auto;
   margin-bottom: 50px;
 }
-
 `;
 
-const AboutPage = () => {
+const ContactPage = () => {
   const pageTitle = (
     <h1>
-      <span>Hello,</span>
-      <span>I'm Ayana.</span>
+      <span>Contact</span>
+      <span></span>
     </h1>
   );
   return (
     <>
-
-      <AboutPageEl>
+      <ContactPageEl>
         <Hero title={pageTitle} />
         <Container>
           <div className="">
             <p>Habitasse, curabitur omnis convallis scelerisque aliqua possimus, totam varius maecenas pretium iusto. Cum quibusdam ea pariatur faucibus habitant animi? Sint voluptates iaculis, lectus rerum esse varius cum iaculis! Totam proident? Consequuntur ipsa eiusmod quo metus praesent ipsam minim morbi autem, nostrum est voluptatibus accusamus, consequatur tellus arcu mollit, cillum pede excepteur aliquid, expedita lorem ultrices venenatis facilisi potenti, eligendi quam cursus possimus maxime nesciunt aliqua inceptos. Laborum aliquip mollitia qui. Malesuada porttitor molestie delectus varius. Pellentesque facilisis voluptate nisl eu, id cumque in pellentesque. Primis nostrud bibendum aute luctus sodales, velit ea ultrices euismod! Aliquid congue, nulla arcu quo facilisi.</p>
           </div>
-
         </Container>
-      </AboutPageEl>
+      </ContactPageEl>
     </>
-  )
+  );
 }
 
-export default AboutPage;
+export default ContactPage
